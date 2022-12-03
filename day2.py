74f31c745d9b12881704c709039b5d0ffc5a97b3
lukebluetiger <lukebluetiger@gmail.com>
2022-12-02 21:01:26 -0600

    day 2 start

---
+++ day2.py
@@ -0,0 +1,10 @@
+score = 0
+input = open('day2.txt', 'r')
+lines = input.readlines()
+for line in lines:
+    line = list(line)
+    if str(line[0]) == 'A':
+        
+    elif str(line[0]) == 'B':
+    
+    elif str(line[0]) == 'C':
