15641b1978e035fd827b4bac19e875839bc8eb6d
lukebluetiger <79377681+lukebluetiger@users.noreply.github.com>
2022-12-03 05:25:35 +0000

    day 2 more

--- day2.py
+++ day2.py
@@ -1,10 +1,38 @@
 score = 0
 input = open('day2.txt', 'r')
 lines = input.readlines()
+
 for line in lines:
     line = list(line)
     if str(line[0]) == 'A':
-        
+      if str(line[2] == 'X'): 
+        score += 1
+        score += 3
+      elif str(line[2] == 'Y'): 
+        score += 2
+        score += 6
+      elif str(line[2] == 'Z'):
+        score += 3
+        score += 0
     elif str(line[0]) == 'B':
-    
+      if str(line[2] == 'X'): 
+        score += 1
+        score += 0
+      elif str(line[2] == 'Y'): 
+        score += 2
+        score += 3
+      elif str(line[2] == 'Z'):
+        score += 3
+        score += 6
     elif str(line[0]) == 'C':
+      if str(line[2] == 'X'): 
+        score += 1
+        score += 6
+      elif str(line[2] == 'Y'): 
+        score += 2
+        score += 0
+      elif str(line[2] == 'Z'):
+        score += 3
+        score += 3
+
+print(score)
